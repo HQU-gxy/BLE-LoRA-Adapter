dad2827520c4fc9e6c85790e919b1091602daca3
Crosstyan <cf595833532@gmail.com>
2023-11-03 21:20:03 +0800

    docs: sprintHex

--- main/include/utils.h
+++ main/include/utils.h
@@ -9,6 +9,16 @@
 
 namespace utils {
 
+/**
+ * @brief put hex string to out
+ * @param out the output buffer
+ * @param outSize the size of output buffer, will NOT include the null terminator
+ * @param bytes the input bytes
+ * @param size the size of input bytes
+ * @return the length of output string
+ * @note length of output string will not include the null terminator
+ *       and the user should add it manually to the buffer, if null terminator is needed
+ */
 size_t sprintHex(char *out, size_t outSize, const uint8_t *bytes, size_t size);
 
 std::string toHex(const uint8_t *bytes, size_t size);
